405fa8f3fde53a546f28ebcaf1f03859890511b8
Svemil DJUSIC <svemildjusic@Svemils-MacBook-Pro.local>
2018-04-09 17:25:36 +0200

    Initial Commit

---
+++ devslopes-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by Svemil DJUSIC on 4/9/18.
+//  Copyright © 2018 Svemil DJUSIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
